cbaa6af45b3fc79d4703ad64176d943f747a9f51
Vitalii <ananievvitalii10@gmail.com>
2024-09-16 17:11:29 -0700

    Include kitchen_service URLs with namespace in project URLconf

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("kitchen_service.urls", namespace="kitchen_service"))
 ]
 
 if settings.DEBUG:
