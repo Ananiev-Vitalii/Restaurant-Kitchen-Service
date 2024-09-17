1111c2f805b2d1242cac6590b237b56d9a0b046b
Vitalii <ananievvitalii10@gmail.com>
2024-09-16 17:50:25 -0700

    Optimize database queries in DishListView with prefetch_related

--- kitchen_service/views.py
+++ kitchen_service/views.py
@@ -7,3 +7,4 @@ class DishListView(generic.ListView):
     model = Dish
     template_name = "kitchen_service/index.html"
     context_object_name = "dish_list"
+    queryset = Dish.objects.all().prefetch_related("cooks", "ingredients")
