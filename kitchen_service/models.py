1fcf3f8484ef4f16272997da9d60d96c9b4ad53d
Vitalii <ananievvitalii10@gmail.com>
2024-09-18 17:47:08 -0700

    Updated models and directory structure for media storage
    - Added new fields to models:
      - `Cook`: Added `photo` field with `ImageField`, uploading to "cooks/" directory.
      - `Dish`: Added `meal_time` field with choices for Breakfast, Lunch, and Dinner, and `image` field with `ImageField`, uploading to "dishes/" directory.
    - Defined `MEAL_TIMES` choices in `Dish` model to categorize dishes by meal time.
    - Created 'dishes' and 'cooks' subdirectories in the media folder to better organize uploaded images.
    - Installed Pillow to support image processing capabilities.
    - Generated and applied migrations to incorporate these changes into the database schema.

--- kitchen_service/models.py
+++ kitchen_service/models.py
@@ -7,6 +7,7 @@ from django.utils.crypto import get_random_string
 
 class Cook(AbstractUser):
     years_of_experience = models.IntegerField(default=0)
+    photo = models.ImageField(upload_to="cooks/", blank=True, null=True)
     facebook_link = models.URLField(
         max_length=255,
         blank=True,
@@ -48,6 +49,12 @@ class DishType(models.Model):
 
 
 class Dish(models.Model):
+    MEAL_TIMES = [
+        ("BF", "Breakfast"),
+        ("LN", "Lunch"),
+        ("DN", "Dinner"),
+    ]
+
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=8, decimal_places=2)
@@ -57,6 +64,8 @@ class Dish(models.Model):
     ingredients = models.ManyToManyField(
         Ingredient, related_name="used_in_dishes")
     is_popular = models.BooleanField(default=False)
+    meal_time = models.CharField(max_length=2, choices=MEAL_TIMES, default="LN")
+    image = models.ImageField(upload_to="dishes/", blank=True, null=True)
 
     def __str__(self):
         return self.name
