e5c4d7191d4ce0852fbe71e077b0f9ae2d07ba88
Vitalii <ananievvitalii10@gmail.com>
2024-09-19 18:52:26 -0700

    Enhanced Team Section in index.html and Added Social Media Links
    - Updated the Team Members section in index.html to display information about our cooks dynamically.
    - Integrated `all_cooks` template tag from `cook_tags`, which filters out superusers, ensuring only regular cooks are displayed.
    - Added `target="_blank"` to social media links to ensure they open in new tabs, improving user engagement and staying on the current page.
    - Ensured social media icons are correctly linked to respective profiles of each cook, enhancing the connectivity of our team's public profiles.

--- kitchen_service/templatetags/cook_tags.py
+++ kitchen_service/templatetags/cook_tags.py
@@ -6,4 +6,10 @@ register = template.Library()
 
 @register.simple_tag
 def total_cooks_count():
-    return Cook.objects.count()
+    return Cook.objects.filter(is_superuser=False).count()
+
+
+@register.simple_tag
+def all_cooks():
+    cooks = Cook.objects.filter(is_superuser=False)
+    return cooks
