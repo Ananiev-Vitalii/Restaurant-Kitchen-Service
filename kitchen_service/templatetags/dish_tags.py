20473df33c07d088fa5099626d7fa51f158456e7
Vitalii <ananievvitalii10@gmail.com>
2024-09-24 18:18:11 -0700

    Added prefetch_related("cooks", "ingredients") to popular_breakfasts and popular_lunches for query optimization. Fixed filtering by meal_time for correct display of popular dishes.

--- kitchen_service/templatetags/dish_tags.py
+++ kitchen_service/templatetags/dish_tags.py
@@ -6,13 +6,17 @@ register = template.Library()
 
 @register.simple_tag
 def popular_breakfasts():
-    breakfasts = Dish.objects.filter(is_popular=True, meal_time="BF")
+    breakfasts = Dish.objects.filter(
+        is_popular=True, meal_time="BF"
+    ).prefetch_related("cooks", "ingredients")
     return breakfasts
 
 
 @register.simple_tag
 def popular_lunches():
-    lunch = Dish.objects.filter(is_popular=True, meal_time="LN")
+    lunch = Dish.objects.filter(
+        is_popular=True, meal_time="LN"
+    ).prefetch_related("cooks", "ingredients")
     return lunch
 
 
