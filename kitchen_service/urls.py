ea0b55cbd0d054f5da034b68b41e4276d21f7a02
Vitalii <ananievvitalii10@gmail.com>
2024-09-24 19:25:08 -0700

    Created AboutTemplateView for about.html page, added 'about/' path in urls.py, created about.html, and uploaded necessary images and videos for the page.

--- kitchen_service/urls.py
+++ kitchen_service/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import DishListView
+from .views import DishListView, AboutTemplateView
 
 urlpatterns = [
-    path("", DishListView.as_view(), name="home")
+    path("", DishListView.as_view(), name="home"),
+    path("about/", AboutTemplateView.as_view(), name="about")
 ]
 
 app_name = "kitchen_service"
