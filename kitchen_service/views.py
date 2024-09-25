c968777215e0e7c521bd7e7233ffe9d4379d149a
Vitalii <ananievvitalii10@gmail.com>
2024-09-24 19:25:08 -0700

    Created AboutTemplateView for about.html page, added 'about/' path in urls.py, created about.html, and uploaded necessary images and videos for the page.

--- kitchen_service/views.py
+++ kitchen_service/views.py
@@ -8,3 +8,12 @@ class DishListView(generic.ListView):
     template_name = "kitchen_service/index.html"
     context_object_name = "dish_list"
     queryset = Dish.objects.all().prefetch_related("cooks", "ingredients")
+
+
+class AboutTemplateView(generic.TemplateView):
+    template_name = "kitchen_service/about.html"
+    extra_context = {
+        "dish_list": Dish.objects.all().prefetch_related(
+            "cooks", "ingredients"
+        )
+    }
