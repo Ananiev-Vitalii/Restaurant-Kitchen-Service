41fe53d221574aa745152f0f34e73971af4fd5d7
Vitalii <ananievvitalii10@gmail.com>
2024-09-24 18:09:24 -0700

    Feature: Updated all_cooks tag to support limit and display all cooks
    - Refactored @register.simple_tag to @register.simple_tag(takes_context=True) to access the request context and object.
    - Added a limit parameter to restrict the number of cooks displayed by default.
    - Added a show_all_cooks check in GET parameters to display the full list of cooks when the "View More" button is clicked.
    - Used prefetch_related("cooked_dishes") to optimize related data fetching and reduce the number of database queries.

--- kitchen_service/templatetags/cook_tags.py
+++ kitchen_service/templatetags/cook_tags.py
@@ -9,7 +9,15 @@ def total_cooks_count():
     return Cook.objects.filter(is_superuser=False).count()
 
 
-@register.simple_tag
-def all_cooks():
-    cooks = Cook.objects.filter(is_superuser=False)
+@register.simple_tag(takes_context=True)
+def all_cooks(context, limit=None):
+    request = context["request"]
+    show_all = request.GET.get("show_all_cooks")
+    cooks = Cook.objects.filter(
+        is_superuser=False
+    ).prefetch_related("cooked_dishes")
+    if show_all:
+        return cooks
+    if limit is not None:
+        return cooks[:limit]
     return cooks
