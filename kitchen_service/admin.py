19652bdd280cf9f78abddece6c5e8e603b4388bf
Vitalii <ananievvitalii10@gmail.com>
2024-11-02 11:56:57 -0700

    Updated Admin Models for Improved Functionality:
    - Added formfield_for_foreignkey in OrderAdmin to filter cooks based on is_cook=True.
    
    - Enhanced DishAdmin with formfield_for_manytomany to filter cooks by is_cook=True.
    
    - Refactored CookAdmin to dynamically adjust fieldsets in admin panel based on is_cook value.
    
    - Removed redundant code for filtering cooks in get_cooks method in DishAdmin.
    
    - Improved typing annotations in admin methods for better type checking and readability.

--- kitchen_service/admin.py
+++ kitchen_service/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 from .models import Cook, Ingredient, DishType, Dish, Order
 from django.contrib.auth.admin import UserAdmin
 from django.db.models import QuerySet
 from django.http import HttpRequest
+from django.db.models import ForeignKey, ManyToManyField
+from typing import Optional, List, Tuple, Any
+from django import forms
 
 
 @admin.register(Ingredient)
@@ -27,11 +31,24 @@ class CookAdmin(UserAdmin):
 
     list_filter = UserAdmin.list_filter + ("is_cook", "years_of_experience")
 
-    fieldsets = UserAdmin.fieldsets + (
-        ("Additional info", {
-            "fields": photo_field + additional_info_fields + social_links_fields
-        }),
-    )
+    def get_fieldsets(
+            self, request: HttpRequest, obj: Optional[Any] = None
+    ) -> List[Tuple[str, dict]]:
+        fieldsets = list(super().get_fieldsets(request, obj))
+
+        if obj and obj.is_cook:
+            fieldsets.append(
+                ("Additional info", {
+                    "fields": self.photo_field + self.additional_info_fields + self.social_links_fields
+                }),
+            )
+        else:
+            fieldsets.append(
+                ("Additional info", {
+                    "fields": ("is_cook",)
+                }),
+            )
+        return fieldsets
 
 
 @admin.register(Dish)
@@ -48,16 +65,22 @@ class DishAdmin(admin.ModelAdmin):
             "cooks", "ingredients"
         )
 
+    def formfield_for_manytomany(
+            self, db_field: ManyToManyField, request: HttpRequest, **kwargs
+    ) -> Optional[forms.ModelMultipleChoiceField]:
+        if db_field.name == "cooks":
+            kwargs["queryset"] = get_user_model().objects.filter(is_cook=True)
+        return super().formfield_for_manytomany(db_field, request, **kwargs)
+
     def get_cooks(self, obj: Dish) -> str:
         return ", ".join([cook.username for cook in obj.cooks.filter(is_cook=True)])
-
-    get_cooks.short_description = "Cooks"
 
     def get_ingredients(self, obj: Dish) -> str:
         return ", ".join(
             [ingredient.name for ingredient in obj.ingredients.all()]
         )
 
+    get_cooks.short_description = "Cooks"
     get_ingredients.short_description = "Ingredients"
 
 
@@ -68,8 +91,15 @@ class OrderAdmin(admin.ModelAdmin):
         "quantity", "total_price", "cook", "status"
     ]
     search_fields = ["order_number", "customer_name"]
-    search_help_text = "Search by order_number  or customer_name"
+    search_help_text = "Search by order_number or customer_name"
     list_filter = ["status"]
 
     def get_queryset(self, request: HttpRequest) -> QuerySet:
         return super().get_queryset(request).prefetch_related("dishes")
+
+    def formfield_for_foreignkey(
+            self, db_field: ForeignKey, request: HttpRequest, **kwargs
+    ) -> Optional[forms.ModelChoiceField]:
+        if db_field.name == "cook":
+            kwargs["queryset"] = get_user_model().objects.filter(is_cook=True)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
