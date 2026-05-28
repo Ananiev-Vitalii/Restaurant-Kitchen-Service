c0efa2387aeb3fd9d2d51f17a0f0a6732315de85
Vitalii <ananievvitalii10@gmail.com>
2026-05-28 03:52:32 -0700

    chore: update dev.py database configuration

--- restaurant/settings/dev.py
+++ restaurant/settings/dev.py
@@ -4,6 +4,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
