e10105761caa414d601f1c055a94aff0149792fd
Vitalii <ananievvitalii10@gmail.com>
2026-05-28 03:45:17 -0700

    chore: update dev.py and base.py database configuration

--- restaurant/settings/base.py
+++ restaurant/settings/base.py
@@ -83,6 +83,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
